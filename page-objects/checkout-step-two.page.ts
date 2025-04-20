22721edb4604391c64c5bbd0bfe55cb2beb55c02
ireichman <isreli@ireichman.com>
2025-04-20 01:58:04 -0600

    Started checkout-step-two.page.ts

---
+++ page-objects/checkout-step-two.page.ts
@@ -0,0 +1,8 @@
+import { Page, Locator } from "@playwright/test";
+import { BasePage } from "./base.page";
+import { UI_BASE_URL } from "../fixtures/test.fixtures";
+
+
+export class CheckoutStepTwoPage extends BasePage {
+    
+}
